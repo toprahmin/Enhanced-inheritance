5ee596fa8f1be5fdccea5d4f85e30e67190013f6
Rahmir Ford <rahmir.ford@gmail.com>
2017-05-03 09:54:45 -0400

    Add files via upload

---
+++ productInventory/src/main/java/Inventory.java
@@ -0,0 +1,7 @@
+/**
+ * Created by rahmirford on 5/3/17.
+ */
+public class Inventory {
+    double totalProductValue;
+    int totalProductQuantity;
+}
---
+++ productInventory/src/test/java/TestProducts.java
@@ -0,0 +1,5 @@
+/**
+ * Created by rahmirford on 5/3/17.
+ */
+public class TestProducts {
+}
